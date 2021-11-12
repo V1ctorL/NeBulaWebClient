8262049a89b0334fab12687bf633dc0d5e8ccdc8
kenora <kenora@localhost>
2021-11-12 18:49:13 +0800

    feat: add Pong
    use SOAP

---
+++ src/java/team/v1ctorl/nebula/Pong.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team.v1ctorl.nebula;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author kenora
+ */
+@WebService(serviceName = "Pong")
+public class Pong {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "pong")
+    public long pong(@WebParam(name = "timestamp") long timestamp) {
+        return System.currentTimeMillis();
+    }
+}
